60fea36e2541d0dfa5892775584e6c6e972aec08
Maulana Agus Setiawan <maulanaa.agss@gmail.com>
2023-11-16 16:40:00 +0800

    Fixed Profile

--- pages/Profile.php
+++ pages/Profile.php
@@ -3,6 +3,10 @@ session_start();
 require 'koneksi.php';
 $username1 = $_SESSION['username'];
 
+if(!isset($_SESSION['username'])){
+    header("Location: Login.php");
+    exit;
+}
 
 if(isset($_POST['submit'])){
     $username = $_POST["username"];
@@ -12,16 +16,17 @@ if(isset($_POST['submit'])){
     $result  = mysqli_query($conn,"SELECT username from akun WHERE username = '$username' ");
     if(mysqli_fetch_assoc($result)){
         echo "<script> alert('username sudah ada !!!');
-        document.location.href='Register.php';
+        document.location.href='Profile.php';
         </script>";
     }else{
         $sql = "UPDATE akun SET password = '$password', username = '$username' WHERE username = '$username1'";
         $result_query = mysqli_query($conn,$sql);
 
         if(mysqli_affected_rows($conn) > 0){
-            echo "<script> alert('Data Berhasil diubah!!');
-        document.location.href='../index.php';
-        </script>";
+            session_destroy();
+            echo "<script> alert('Data Berhasil diubah!! Silahkan login Kembali');
+            document.location.href='Login.php';
+            </script>";
         }else{
             echo "<script> alert('Data gagal diubah!!');
             document.location.href='Profile.php';
